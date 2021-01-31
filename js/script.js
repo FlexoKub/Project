367285fde200479a9c672d9e56132208d1287e3f
Sergey <FlexoKub2@yandex.ru>
2021-01-31 19:18:07 +0800

    Добавлены файлы в js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let money = '200 руб';
+let income = true;
+let addExpenses = 'JavaScript';
+let deposit = '№ 256332';
+let mission = 25;
+let period = '27.01.2021';
+
+alert(addExpenses);
+console.log('addExpenses: ', addExpenses);
+console.log(typeof(mission));
